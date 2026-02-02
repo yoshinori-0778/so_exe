f558a82005aaa719ee7779715e2ec999597695dd
Yoshinori Sueno <ys5857@tiger3.princeton.edu>
2026-02-02 17:02:38 -0500

    add accl_vel

---
+++ hkdata/vel_accl.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import datetime
+import sys, os
+from tqdm import tqdm
+from scipy.interpolate import interp1d
+
+from sotodlib import core, tod_ops
+from sotodlib.io import hkdb
+from sotodlib.utils.procs_pool import get_exec_env
+
+sys.path.append('/home/ys5857/workspace/script/so_ana/')
+from config import *
+from util.misc import save_pkl
+from util.misc import get_vel_accl
+
+def main(obsid):
+    ctx1 = core.Context(CTX_PATH1)
+    platform = 'satp1'
+    azvel, azacl = get_vel_accl(ctx1, obsid, platform)
+    #print(azvel, azacl)
+    save_pkl((azvel,azacl), f'/scratch/gpfs/SIMONSOBS/users/ys5857/workspace/output/vel_accl/{platform}/{obsid}.pkl')
+    return
+
+
+def main_multiprocess(executor, as_completed_callable, ctx, ys, ms, ye, me, platform):
+    scan_start = datetime.datetime(ys, ms, 1, 0, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    scan_stop = datetime.datetime(ye, me, 1, 0, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    obslist = ctx.obsdb.query(f'timestamp > {scan_start.timestamp()} and timestamp < {scan_stop.timestamp()}')
+    obss = []
+    for iobs in obslist:
+        #print(iobs['obs_id'], ctx.obsdb.get(iobs['obs_id'], tags=True)['tags'])
+        if ctx.obsdb.get(iobs['obs_id'], tags=True)['subtype'] == 'cmb':
+            obss.append(iobs['obs_id'])
+    print(f'{scan_start} - {scan_stop}: Length = {len(obss)}')
+
+    runlist = []
+    for iobsid in obss:
+        irunlist = {'obsid': iobsid}
+        runlist.append(irunlist)
+    n_runs = len(runlist)
+    print(f'number of runs: {n_runs}')
+    future_to_rl = {executor.submit(main, obsid=rl['obsid']): rl for rl in runlist}
+    futures = list(future_to_rl)
+
+    n = 0
+    for future in as_completed_callable(futures):
+        rl = future_to_rl[future]
+        try:
+            n += 1
+            future.result()
+            futures.remove(future)
+            print(f'Processing Finished correctly {n}/{n_runs}')
+        except Exception as e:
+            print(f'Processing {n}/{n_runs} generated an exception: {e}')
+            futures.remove(future)
+
+
+if __name__ == '__main__':
+    # SATP3 yaml
+    #CTX_PATH3 = '/so/metadata/satp3/contexts/use_this.yaml'
+    #CTX3 = core.Context(CTX_PATH3)
+    #CTX_PATH1 = '/so/metadata/satp1/contexts/use_this.yaml'
+    #CTX1 = core.Context(CTX_PATH1)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ys', type=int)
+    parser.add_argument('ms', type=int)
+    parser.add_argument('ye', type=int)
+    parser.add_argument('me', type=int)
+    parser.add_argument('--sat', type=str, default='satp1', help='Start year for analysis')
+    parser.add_argument('--nproc', type=int, default=2, help='Number of processes to use')
+    args = parser.parse_args()
+    rank, executor, as_completed_callable = get_exec_env(args.nproc)
+    if args.sat == 'satp3':
+        CTX_PATH3 = '/scratch/gpfs/SIMONSOBS/so/tracked/metadata/satp3/contexts/use_this_local.yaml'
+        ctx = core.Context(CTX_PATH3)
+        platform = 'satp3'
+    elif args.sat == 'satp1':
+        CTX_PATH1 = '/scratch/gpfs/SIMONSOBS/so/tracked/metadata/satp1/contexts/use_this_local.yaml'
+        ctx = core.Context(CTX_PATH1)
+        platform = 'satp1'
+    else:
+        raise ValueError('Platform not recognized, please use satp1 or satp3')
+
+    if rank == 0:
+        main_multiprocess(executor, as_completed_callable, ctx, args.ys, args.ms, args.ye, args.me, platform=platform)
+        
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ys', type=int)
+    parser.add_argument('ms', type=int)
+    parser.add_argument('ye', type=int)
+    parser.add_argument('me', type=int)
+    parser.add_argument('--sat', type=str, default='satp1', help='Start year for analysis')
+    args = parser.parse_args()
+    if args.sat == 'satp3':
+        CTX_PATH3 = '/scratch/gpfs/SIMONSOBS/so/tracked/metadata/satp3/contexts/use_this_local.yaml'
+        ctx = core.Context(CTX_PATH3)
+        platform = 'satp3'
+    elif args.sat == 'satp1':
+        CTX_PATH1 = '/scratch/gpfs/SIMONSOBS/so/tracked/metadata/satp1/contexts/use_this_local.yaml'
+        ctx = core.Context(CTX_PATH1)
+        platform = 'satp1'
+    else:
+        raise ValueError('Platform not recognized, please use satp1 or satp3')
+
+    main(ctx, args.ys, args.ms, args.ye, args.me, platform=platform)
+    """
+    """
+    div_set = [
+        #[2023,10,2023,11],
+        #[2023,11,2023,12],
+        #[2023,12,2024,1],
+        #[2024,1,2024,2],
+        #[2024,2,2024,3],
+        #[2024,3,2024,4],
+        #[2024,4,2024,5],
+        #[2024,5,2024,6],
+        #[2024,6,2024,7],
+        #[2024,7,2024,8],
+        [2024,8,2024,9],
+        [2024,9,2024,10],
+        [2024,10,2024,11],
+        [2024,11,2024,12],
+        [2024,12,2025,1],
+        [2025,1,2025,2],
+        ]
+    """
+    #for ys, ms, ye, me in div_set:
+    #    print(ys, ms, ye, me)
+    #    main(CTX3, ys, ms, ye, me, platform='satp3')
+    #   #main(CTX1, ys, ms, ye, me, platform='satp1')
